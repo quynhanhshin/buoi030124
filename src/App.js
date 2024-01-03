0a9567c05deb37d9c60cbfa1449c80802a711551
quynhanhshin <shinanh21@gmail.com>
2024-01-03 20:57:01 +0700

    them header & footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactStrap from './component/ReactStrap';
+import Footer from './component/footer/Footer';
+import "./style.css"
 
 function App() {
   return (
     <div>
       <ReactStrap/>
+      <Footer/>
     </div>
   );
 }
