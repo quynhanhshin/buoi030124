a6e4afd4b106683fb332826ad5bd2846d261b03d
quynhanhshin <shinanh21@gmail>
2024-01-05 20:24:36 +0700

    Dieu huong

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactStrap from './component/ReactStrap';
-import Footer from './component/footer/Footer';
+import Footer from './component/footer/Footer'
 import "./style.css"
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Header from "./component/header/Header"
+import Home from './component/home/Home';
+import Menu from './component/Menu';
+import NotFound from './component/NotFound';
+
 
 function App() {
   return (
     <div>
-      <ReactStrap/>
-      <Footer/>
+      {/* <ReactStrap/>
+      <Footer/> */}
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/footer" element={<Footer/>} />
+        <Route path="/header" element={<Header/>}/>
+        <Route path="/reactstrap" element={<ReactStrap/>}/>
+        <Route path="/menu" element={<Menu/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/Menu.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function () {
+  return (
+    <div>
+        {/* <ul>
+            <li>
+                <a href='Home'>Home</a>
+            </li>
+            <li>
+                <a href='ReactStrap'>ReactStrap</a>
+            </li>
+            <li>
+                <a href='Header'>Header</a>
+            </li>
+            <li>
+                <a href='Footer'>Footer</a>
+            </li>
+        </ul> */}
+         <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/header">Header</Link>
+            </li>
+            <li>
+                <Link to="/footer">Footer</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
